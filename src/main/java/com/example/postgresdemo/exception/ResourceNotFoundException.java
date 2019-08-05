3a35337ce91a8e4969b6fcc290426d978f23d23e
SatyabrataKundu <satyabrata_kundu@persistent.co.in>
2019-08-05 09:53:43 +0530

    first commit

---
+++ src/main/java/com/example/postgresdemo/exception/ResourceNotFoundException.java
@@ -0,0 +1,20 @@
+package com.example.postgresdemo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 8863121217983453538L;
+
+	public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
